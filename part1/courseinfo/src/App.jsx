7a204896043fee52f501256d49cebe28775d7329
snaphuman <fhernandezn@gmail.com>
2024-03-12 13:32:01 -0500

    last refactor to use destructured object as props data

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,28 +4,29 @@ import Total from './components/Total'
 
 const App = () => {
 
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      count: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      count: 7,
-
-    },
-    {
-      name: 'State of a component',
-      count: 14,
-    }
-  ];
+  const { name, parts } = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        count: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        count: 7,
+      },
+      {
+        name: 'State of a component',
+        count: 14,
+      }
+    ]
+  } 
 
   const total = parts.reduce((sum, {count:it}) => sum + it, 0);
 
   return (
     <div>
-      <Header course={ course } />
+      <Header course={ name } />
       <Content parts={ parts } />
       <Total total={ total } />
     </div>

b30ce16245283d92d346d35983e30b93f90a19e9
snaphuman <fhernandezn@gmail.com>
2024-03-12 13:21:49 -0500

    fix course prop name

--- part1/courseinfo/src/components/Header.tsx
+++ part1/courseinfo/src/components/Header.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Header = ({title}) => {
+const Header = ({course}) => {
 
     return (
         <>
-            <h1>{ title }</h1>
+            <h1>{ course }</h1>
         </>
     )
 }
